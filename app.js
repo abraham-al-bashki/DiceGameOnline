4b4e5092cf3b44a8e211ee75c25fcacaae45ba96
Abraham Al-bashki <94388656+halmstad93@users.noreply.github.com>
2024-06-16 17:23:54 +0200

    basically done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
-
+const serverData = [];
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -16,14 +16,47 @@ io.on('connection', (socket) => {
   io.emit('chat message', `${socket.id} is connected`);
   console.log(`${socket.id} is connected`);
   socket.on('disconnect', () => {
+    adjustArray(socket.id);
     io.emit('chat message', `${socket.id} is disconnected`);
     console.log(`${socket.id} is disconnected`);
+    console.log(serverData);
+  });
+  socket.on('meta data', (data) => {
+    data.id = socket.id;
+    data.totalScore = 0;
+    serverData.push(data);
+    console.log(serverData);
   });
   socket.on('chat message', (msg) => {
-    io.emit('chat message', `${socket.id}: ${msg}`);
+    const userObject = serverData.filter((arg) => arg.id === socket.id)[0];
+    console.log('userObject');
+    console.log(userObject);
+    console.log('end');
+    io.emit('chat message', `${userObject.username}: ${msg}`);
+  });
+  socket.on('cast dice', (data) => {
+    console.log('work?');
+    const newScore = Math.floor(Math.random() * 6 + 1);
+    let obj = serverData.find((obj) => obj.username === data.username);
+    if (obj) {
+      obj.totalScore = obj.totalScore + newScore;
+    }
+    console.log(obj);
+    io.emit(
+      'chat message',
+      `${obj.username} cast a ${newScore} and has a total of ${obj.totalScore} played`
+    );
   });
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+function adjustArray(id) {
+  for (let i = serverData.length - 1; i >= 0; i--) {
+    if (id === serverData[i].id) {
+      serverData.splice(i, 1);
+    }
+  }
+}
--- public/index.js
+++ public/index.js
@@ -3,13 +3,22 @@ const socket = io();
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
+const castDicebutton = document.getElementById('cast-dice');
+const data = { username: '' };
+
+checkUsername();
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
+  checkUsername();
   if (input.value) {
     socket.emit('chat message', input.value);
     input.value = '';
   }
+});
+castDicebutton.addEventListener('click', (e) => {
+  e.preventDefault();
+  socket.emit('cast dice', data);
 });
 socket.on('chat message', function (msg) {
   const item = document.createElement('li');
@@ -17,3 +26,42 @@ socket.on('chat message', function (msg) {
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
 });
+
+function checkUsername() {
+  const inputName = document.getElementById('input-name');
+  console.log(data.username === '' && inputName !== null);
+  if (data.username === '' && inputName !== null) {
+    data.username = inputName.value;
+    if (data.username !== '') {
+      socket.emit('meta data', data);
+    }
+  }
+  if (data.username === '') {
+    messages.hidden = true;
+    castDicebutton.hidden = true;
+    form.textContent = '';
+    const labelElement = document.createElement('label');
+    labelElement.setAttribute('for', 'input-name');
+    const labelText = document.createTextNode('Name: ');
+    labelElement.appendChild(labelText);
+    form.appendChild(labelElement);
+    const inputElement = document.createElement('input');
+    inputElement.setAttribute('id', 'input-name');
+    inputElement.setAttribute('placeholder', 'Type name..');
+    inputElement.setAttribute('autocomplete', 'off');
+    form.appendChild(inputElement);
+    const buttonElement = document.createElement('button');
+    const SendButtonText = document.createTextNode('Confirm');
+    buttonElement.appendChild(SendButtonText);
+    form.appendChild(buttonElement);
+  } else {
+    messages.hidden = false;
+    castDicebutton.hidden = false;
+    form.textContent = '';
+    form.appendChild(input);
+    const buttonSubmit = document.createElement('button');
+    const SendButtonText = document.createTextNode('Send');
+    buttonSubmit.appendChild(SendButtonText);
+    form.appendChild(buttonSubmit);
+  }
+}
